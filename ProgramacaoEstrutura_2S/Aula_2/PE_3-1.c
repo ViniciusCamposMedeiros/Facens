7bddac560c3002898d67dca1f323ba749b81b149
VinciusCamposMedeiros <vinicius.campos.medeiros@gmail.com>
2026-08-25 19:27:16 -0300

    primeiro exercício resolvido

---
+++ ProgramacaoEstrutura_2S/Aula_2/PE_3-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamanho();
+void recebe(int *p, int tam);
+void mostra(int *p, int tam);
+
+int main(){
+    int grandeza = tamanho();
+    int vetor[grandeza];
+    recebe(vetor, grandeza); // como não tem index recebe o endereço
+    mostra(vetor, grandeza);
+    system("pause");
+}
+
+int tamanho(){
+    int tam;
+    printf("Digite o tamanho da matriz= ");
+        scanf("%i", &tam);
+    return tam;
+}
+
+void recebe(int *p, int tam){
+    for(int i=0;i<tam;i++,p++){ //p++ aumenta o endereço
+        printf("Valor[%i]= ", i);
+            scanf("%i", p);
+    }
+}
+
+void mostra(int *p, int tam){
+    for(int i=0;i<tam;i++,p++){
+        printf("End[%i]= %i // Valor[%i]= %i\n",i, p, i, *p);
+    }
+}
